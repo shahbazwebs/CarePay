8cd4dbada18624898044a423d9873b4c23e30457
shahbaz.akhtar0217 <shahbaz.webs@gmail.com>
2024-09-14 13:10:10 +0530

    carepay website upload

---
+++ assets/css/style.cc
@@ -0,0 +1,56 @@
+@import url("https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&subset=devanagari,latin-ext");
+@font-face {
+  font-family: "RizkiaAisha";
+  src: url("../fonts/RizkiaAisha.eot");
+  src: url("../fonts/RizkiaAisha.eot") format("embedded-opentype"), url("../fonts/RizkiaAisha.woff2") format("woff2"), url("../fonts/RizkiaAisha.woff") format("woff"), url("../fonts/RizkiaAisha.ttf") format("truetype"), url("../fonts/RizkiaAisha.svg#RizkiaAisha") format("svg");
+}
+html {
+  max-height: 100%;
+  height: 100%;
+}
+
+body {
+  position: relative;
+  overflow-x: hidden;
+  font-family: "Poppins", sans-serif;
+}
+
+::selection {
+  color: #fff;
+  background-color: #6fc03f;
+}
+
+::-moz-selection {
+  color: #fff;
+  background-color: #6fc03f;
+}
+
+.mini_header {
+  position: relative;
+  width: 100%;
+  background-color: #6fc03f;
+}
+.mini_header .contact_informataions {
+  position: relative;
+}
+.mini_header .contact_informataions ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  align-items: center;
+}
+.mini_header .contact_informataions ul li {
+  margin-right: 20px;
+}
+.mini_header .contact_informataions ul li a {
+  color: #FFFFFF;
+}
+.mini_header .contact_informataions ul li a span {
+  font-weight: bold;
+}
+.mini_header .contact_informataions ul li:last-child {
+  margin-right: 0;
+}
+
+/*# sourceMappingURL=style.cc.map */
---
+++ assets/js/main.js
@@ -0,0 +1,69 @@
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Surgery.", "Radiology.", "Pathology and Laboratory.", "Medicine.","Evaluation and Management."];
+const typingDelay = 150;
+const erasingDelay = 70;
+const newTextDelay = 1500; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+	// Testimonial Carousel
+	if ($('.testimonial-carousel').length) {
+		$('.testimonial-carousel').owlCarousel({
+			animateOut: 'slideOutDown',
+		    animateIn: 'zoomIn',
+			loop:true,
+			margin:0,
+			nav:true,
+			smartSpeed: 300,
+			autoplay: 7000,
+			navText: [ '<span class="arrow-left"></span>', '<span class="arrow-right"></span>' ],
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:1
+				},
+				800:{
+					items:1
+				},
+				1024:{
+					items:1
+				}
+			}
+		});  		
+	}
